124c72cf575ae0fef229a814e012586b13f00932
Thiago Batista <thiagobatista15390@gmail.com>
2025-03-02 17:35:14 -0300

    criando o header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,30 @@
+import { HeaderBar, LinkCart, Links, LinksItems } from './styles'
+import logo from '../../assets/images/logo.svg'
+import carrinho from '../../assets/images/carrinho.svg'
+
+const Header = () => (
+  <HeaderBar>
+    <div>
+      <img src={logo} alt="Eplay" />
+      <nav>
+        <Links>
+          <LinksItems>
+            <a href="#">Categorias</a>
+          </LinksItems>
+          <LinksItems>
+            <a href="#">Novidades</a>
+          </LinksItems>
+          <LinksItems>
+            <a href="#">Promoções</a>
+          </LinksItems>
+        </Links>
+      </nav>
+    </div>
+    <LinkCart href="#">
+      0 - produto(s)
+      <img src={carrinho} alt="carrinho" />
+    </LinkCart>
+  </HeaderBar>
+)
+
+export default Header
